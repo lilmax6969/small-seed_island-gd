9882c5eac624e7723c14d7a667220e3c379cfe1f
traktor123 <maxfortino00000@gmail.com>
2025-04-21 12:52:30 -0400

    create movement for the player

--- scene/character_body_2d.gd
+++ scene/character_body_2d.gd
@@ -1,11 +1,15 @@
 extends CharacterBody2D
 
+@export var speed: int = 400
 
-# Called when the node enters the scene tree for the first time.
+func get_input():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	
+	velocity = input_direction * speed
+
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
 	pass
+
+func _physics_process(delta: float) -> void:
+	get_input()
+	move_and_slide()
